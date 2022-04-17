485e60ba654f8a0d98ad66432e1708529215fe6c
Samuel Chapuis <103772966+Thorid4n@users.noreply.github.com>
2022-04-17 17:05:00 +0200

    Update main.c

--- main.c
+++ main.c
@@ -17,7 +17,6 @@
 
 
 /* ------- Déclaration des sous programmes ------- */
-#include "lib/color.h"
 #include "lib/generation.h"
 #include "definition.h"
 #include "lib/graphic.h"
@@ -36,22 +35,65 @@ int main(int argc, char **argv)
     /** IMPORTANT EN FONCTION DU NOMBRE DE JOEUEUR LA GENERATION DE LA CARTE PEUX ECHOUER IL EST DONC TRES FORTEMEN DECONSEILLER
     DE GENERER UNE CARTE D'UNE TAILLE INFERIEUR A 2^3 **/
 
-    SDL_Window* fennetre = NULL;
-    SDL_Renderer* rendu = NULL;
-
-
-    fennetre = SDL_CreateWindow("TestWidow", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,542, 542, SDL_WINDOW_SHOWN);
-    rendu = SDL_CreateRenderer(fennetre,-1,SDL_RENDERER_ACCELERATED);
-
-
     hauteur_max=2.9;
     coef_aleatoire=1.9;
-    taille_carte=4;
+    taille_carte=5;
+
+/* Initialisation de la fenètres de debuging */
+
+    SDL_Window* fenetre_debug = NULL;
+    SDL_Renderer* rendu_debug = NULL;
+
+    fenetre_debug = SDL_CreateWindow("debug", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN /*| SDL_WINDOW_RESIZABLE */);
+    rendu_debug = SDL_CreateRenderer(fenetre_debug,-1,SDL_RENDERER_ACCELERATED);
+
+    SDL_Surface* icone_debug = NULL;
+    icone_debug = SDL_LoadBMP("sprite/bug.bmp");
+    SDL_SetWindowIcon(fenetre_debug, icone_debug);
+    SDL_FreeSurface(icone_debug);
+
+/*--------------- ---------------*/
+
 
 /* Defition du nombre de joueurs et de la taille de la carte relativement à ça */
 
 
 /*--------------- ---------------*/
+
+
+/* Initialisation des fenètres de jeux */
+
+    int carte_x = 1055;
+    int carte_y = 800;
+
+    SDL_Window* fenetre_carte = NULL;
+    SDL_Renderer* rendu_carte = NULL;
+
+    fenetre_carte = SDL_CreateWindow("TEMPUS", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, carte_x, carte_y, SDL_WINDOW_SHOWN /*| SDL_WINDOW_RESIZABLE */);
+    rendu_carte = SDL_CreateRenderer(fenetre_carte,-1,SDL_RENDERER_ACCELERATED);
+
+    SDL_Surface* icone_carte = NULL;
+    icone_carte = SDL_LoadBMP("sprite/chevalier_rouge.bmp");
+    SDL_SetWindowIcon(fenetre_carte, icone_carte);
+    SDL_FreeSurface(icone_carte);
+
+
+    SDL_Window* fenetre_info_case = NULL;
+    SDL_Renderer* rendu_info_case = NULL;
+
+    fenetre_info_case = SDL_CreateWindow("Info Case", 1.45*carte_x, SDL_WINDOWPOS_CENTERED,200 , 800, SDL_WINDOW_SHOWN /*| SDL_WINDOW_RESIZABLE */);
+    rendu_info_case = SDL_CreateRenderer(fenetre_info_case,-1,SDL_RENDERER_ACCELERATED);
+
+    SDL_Surface* icone_info_case = NULL;
+    icone_info_case = SDL_LoadBMP("sprite/parchemin.bmp");
+    SDL_SetWindowIcon(fenetre_info_case, icone_info_case);
+    SDL_FreeSurface(icone_case);
+
+
+/*--------------- ---------------*/
+
+
+
 
 
 /* Definition des valeurs des objets relative au paramèter de la partie */
@@ -90,7 +132,7 @@ int main(int argc, char **argv)
             return error;
         }
 
-    graphic_plateau(carte,taille_carte,rendu);
+    graphic_plateau(carte,taille_carte,rendu_carte);
 
 
 /*--------------- ---------------*/
@@ -106,7 +148,28 @@ int main(int argc, char **argv)
 
 /*--------------- ---------------*/
 
-
+    int isrunning = 1;
+    SDL_Event ev;
+    while(isrunning)
+    {
+        while(SDL_PollEvent(&ev) != 0);
+        {
+            if(ev.type == SDL_QUIT)
+            isrunning = 0;
+            else if (ev.type == SDL_KEYDOWN)
+            {
+                switch(ev.key.keysym.sym)
+                {
+                case SDLK_F11:
+                    return 0;
+                    break;
+                case SDLK_z:
+                    SDL_Delay(500);
+                    break;
+                }
+            }
+        }
+    }
 
 
 
@@ -123,13 +186,28 @@ int main(int argc, char **argv)
 
 /*--------------- ---------------*/
 
-    SDL_Delay(50000);
+
+    SDL_Event event;
+    SDL_WaitEvent(&event);
+    SDL_bool continuer = SDL_FALSE;
+    while(continuer)
+    {
+        SDL_WaitEvent(&event);
+        if(event.type == SDL_QUIT)
+            continuer = SDL_FALSE;
+        if(event.type == SDL_KEYDOWN)
+            printf("appui sur une touche");
+    }
 
 /* Fin du programe */
-
-    SDL_DestroyRenderer(rendu);
-    SDL_DestroyWindow(fennetre);
+    SDL_DestroyRenderer(rendu_debug);
+    SDL_DestroyWindow(fenetre_debug);
+    SDL_DestroyRenderer(rendu_carte);
+    SDL_DestroyWindow(fenetre_carte);
+    SDL_DestroyRenderer(rendu_case);
+    SDL_DestroyWindow(fenetre_case);
     SDL_Quit();
+
 
     return 0;
 /*--------------- ---------------*/
