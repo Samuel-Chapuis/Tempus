2a7ab19bed167243b5fe8259784ee489a7d68333
Samuel Chapuis <103772966+Thorid4n@users.noreply.github.com>
2022-04-22 17:07:52 +0200

    Update main.c

--- main.c
+++ main.c
@@ -21,41 +21,34 @@
 #include "definition.h"
 #include "lib/graphic.h"
 #include "lib/erreur.h"
+#include "lib/color.h"
+#include "lib/evenement.h"
+
+#include "lib/test.h"
 
 static int error=-1;
 
-
-
 int main(int argc, char **argv)
 {
-    unsigned int hauteur_max=2.9;
-    unsigned int coef_aleatoire=1.9;
-    unsigned int taille_carte=0;
-    unsigned int nb_joeurs=4; /// Peut aller jusqu'a 6 !
-    /** IMPORTANT EN FONCTION DU NOMBRE DE JOEUEUR LA GENERATION DE LA CARTE PEUX ECHOUER IL EST DONC TRES FORTEMEN DECONSEILLER
-    DE GENERER UNE CARTE D'UNE TAILLE INFERIEUR A 2^3 **/
 
-    hauteur_max=2.9;
-    coef_aleatoire=1.9;
-    taille_carte=5;
+/* Initialisation de la console qui permetra de debugger */
 
     AllocConsole();
- 
-/* Initialisation de la fenètres de debuging */
-
-    SDL_Window* fenetre_debug = NULL;
-    SDL_Renderer* rendu_debug = NULL;
-
-    fenetre_debug = SDL_CreateWindow("debug", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN /*| SDL_WINDOW_RESIZABLE */);
-    rendu_debug = SDL_CreateRenderer(fenetre_debug,-1,SDL_RENDERER_ACCELERATED);
-
-    SDL_Surface* icone_debug = NULL;
-    icone_debug = SDL_LoadBMP("sprite/bug.bmp");
-    SDL_SetWindowIcon(fenetre_debug, icone_debug);
-    SDL_FreeSurface(icone_debug);
+    /// Appuyer sur F10 doit permettre d'afficher la console sinon suprimé la ligne si dessous ///
+    ShowWindow(GetConsoleWindow(),SW_HIDE);
+    int console = 0;
 
 /*--------------- ---------------*/
 
+
+    unsigned int hauteur_max=2.9;
+    unsigned int coef_aleatoire=1.9;
+    unsigned int taille_carte=0;
+    unsigned int nb_joeurs=6; /// Peut aller jusqu'a 6 !
+    /** IMPORTANT EN FONCTION DU NOMBRE DE JOEUEUR LA GENERATION DE LA CARTE PEUX ECHOUER IL EST DONC TRES FORTEMEN DECONSEILLER
+    DE GENERER UNE CARTE D'UNE TAILLE INFERIEUR A 2^3 **/
+
+    taille_carte=5;
 
 /* Defition du nombre de joueurs et de la taille de la carte relativement à ça */
 
@@ -83,14 +76,8 @@ int main(int argc, char **argv)
     SDL_Window* fenetre_info_case = NULL;
     SDL_Renderer* rendu_info_case = NULL;
 
-    fenetre_info_case = SDL_CreateWindow("Info Case", 1.45*carte_x, SDL_WINDOWPOS_CENTERED,200 , 800, SDL_WINDOW_SHOWN /*| SDL_WINDOW_RESIZABLE */);
+    fenetre_info_case = SDL_CreateWindow("Info Case", 1.25*carte_x, SDL_WINDOWPOS_CENTERED,225 , 330, SDL_WINDOW_SHOWN | SDL_WINDOW_BORDERLESS);
     rendu_info_case = SDL_CreateRenderer(fenetre_info_case,-1,SDL_RENDERER_ACCELERATED);
-
-    SDL_Surface* icone_info_case = NULL;
-    icone_info_case = SDL_LoadBMP("sprite/parchemin.bmp");
-    SDL_SetWindowIcon(fenetre_info_case, icone_info_case);
-    SDL_FreeSurface(icone_case);
-
 
 /*--------------- ---------------*/
 
@@ -103,6 +90,7 @@ int main(int argc, char **argv)
     coef_aleatoire=coef_aleatoire*(pow(2,taille_carte)+1)/2;
     hauteur_max=hauteur_max*(pow(2,taille_carte)+1)/2;
     taille_carte=pow(2,taille_carte)+1;
+
 /*--------------- ---------------*/
 
 
@@ -126,8 +114,8 @@ int main(int argc, char **argv)
 
 /* Initialisation du plateau de jeu */
 
-    generationcarte(carte,taille_carte,coef_aleatoire,hauteur_max,nb_joeurs);
-    error = generationposition(carte,taille_carte,coef_aleatoire,hauteur_max,nb_joeurs);
+    generationcarte(carte, taille_carte, coef_aleatoire, hauteur_max, nb_joeurs);
+    error = generationposition(carte, taille_carte, coef_aleatoire, hauteur_max,nb_joeurs);
     if(error!=0)
         {
             erreur(error);
@@ -141,36 +129,35 @@ int main(int argc, char **argv)
 
 
 /* Initialisation des tours */
+    joueur* player_tab;
+    player_tab=(joueur*)malloc(nb_joeurs*sizeof(joueur));
+///Permet de verifier l'allocation si dessus
+    if(player_tab == NULL)
+    {
+        erreur(1001);
+        return 1001;
+    }
 
-    joueur player_tab[nb_joeurs];
     for(int i=1; i<=nb_joeurs; i++)
     {
         player_tab[i].couleur=i;
+        player_tab[i].nb_villes=1;
+        player_tab[i].nb_habitants=1;
+        player_tab[i].science_total=0;
     }
 
 /*--------------- ---------------*/
 
+
+
+
     int isrunning = 1;
-    SDL_Event ev;
+
+    console = 0;
+
     while(isrunning)
     {
-        while(SDL_PollEvent(&ev) != 0);
-        {
-            if(ev.type == SDL_QUIT)
-            isrunning = 0;
-            else if (ev.type == SDL_KEYDOWN)
-            {
-                switch(ev.key.keysym.sym)
-                {
-                case SDLK_F11:
-                    return 0;
-                    break;
-                case SDLK_z:
-                    SDL_Delay(500);
-                    break;
-                }
-            }
-        }
+        event(&console, carte, fenetre_carte, rendu_info_case);
     }
 
 
@@ -189,27 +176,12 @@ int main(int argc, char **argv)
 /*--------------- ---------------*/
 
 
-    SDL_Event event;
-    SDL_WaitEvent(&event);
-    SDL_bool continuer = SDL_FALSE;
-    while(continuer)
-    {
-        SDL_WaitEvent(&event);
-        if(event.type == SDL_QUIT)
-            continuer = SDL_FALSE;
-        if(event.type == SDL_KEYDOWN)
-            printf("appui sur une touche");
-    }
-
 /* Fin du programe */
-    SDL_DestroyRenderer(rendu_debug);
-    SDL_DestroyWindow(fenetre_debug);
     SDL_DestroyRenderer(rendu_carte);
     SDL_DestroyWindow(fenetre_carte);
-    SDL_DestroyRenderer(rendu_case);
-    SDL_DestroyWindow(fenetre_case);
+    SDL_DestroyRenderer(rendu_info_case);
+    SDL_DestroyWindow(fenetre_info_case);
     SDL_Quit();
-
 
     return 0;
 /*--------------- ---------------*/
